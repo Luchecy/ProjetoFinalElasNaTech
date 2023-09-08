2cc63c899314e216abcbd27975085efecc40327a
luchecy <luchecyribeiro@outlook.com>
2023-09-07 22:59:54 -0300

    atualizacoes

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menu-btn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+  navbar.classList.toggle("menu-open");
+});
